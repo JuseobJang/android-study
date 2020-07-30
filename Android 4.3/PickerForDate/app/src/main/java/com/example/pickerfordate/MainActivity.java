d1605744dcfbf537b92e551dcd45764d0dfe97cc
JuseobJang <wntjq68@gmail.com>
2020-07-30 21:26:56 +0900

    feat: Add date picker function
    Pressing the "Date" button brings up a fragment for selecting a date. And if you select the desired date, the processDatePickerResult() function in the main activity is executed to display the selected date.

--- Android 4.3/PickerForDate/app/src/main/java/com/example/pickerfordate/MainActivity.java
+++ Android 4.3/PickerForDate/app/src/main/java/com/example/pickerfordate/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.pickerfordate;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showDatePicker(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(), getString(R.string.datepicker));
+    }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month + 1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (month_string + "/" + day_string + "/" + year_string);
+
+        Toast.makeText(this, getString(R.string.date) + dateMessage,
+                Toast.LENGTH_SHORT).show();
     }
 }
