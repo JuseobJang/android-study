d74a1bfbdc375534a2dfbac5278fc3a358ab2198
JuseobJang <wntjq68@gmail.com>
2020-08-20 19:35:48 +0900

    feat: Add jobscheduler constraint
     In this project, job means notification, and the job is executed when the constraints set by the user are satisfied.
    
     1. Network status
        You can select one of None, Any and WIFI by using the radio group. None is any default value and performs the job regardless of any network connection. Any performs work regardless of Wi-Fi and data, and WIFI works only for Wi-Fi except hotspots.
    
     2. Idle & charging
        You can use the switches to set the conditions for idle and charging, respectively.
    
     3. Deadline
        This allows you to set a minimum amount of time to do this.
    
     Each configuration is built using JobInfo.builder and executes onStartJob() of NotificationJobService.class which was previously imported as component Name by JobScheduler.

--- Android 8.3/NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.java
+++ Android 8.3/NotificationScheduler/app/src/main/java/com/example/notificationscheduler/MainActivity.java
@@ -2,13 +2,123 @@ package com.example.notificationscheduler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private JobScheduler mScheduler;
+    private static final int JOB_ID = 0;
+
+    //job option
+    private Switch mDeviceIdleSwitch;
+    private Switch mDeviceChargingSwitch;
+    private SeekBar mSeekBar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
+        mDeviceChargingSwitch = findViewById(R.id.chargingSwitch);
+        mSeekBar = findViewById(R.id.seekBar);
+
+        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
+
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);// scheduler 초기화
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (i > 0){
+                    seekBarProgress.setText(i + " s");
+                }else {
+                    seekBarProgress.setText("Not Set");
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+    }
+
+    public void scheduleJob(View view){
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+
+
+        //라디오 버튼 그룹 중에 선택된 것의 아이디를 인트로 받아옴!
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        // default
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        int seekBarInteger = mSeekBar.getProgress();
+        boolean seekBarSet = seekBarInteger > 0;
+
+        switch (selectedNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE; // network 연결 상관없음
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY; // wifi or data
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED; // only wifi
+                break;
+        }
+
+        // builder 를 사용하여 JobInfo 설정
+        ComponentName serviceName = new ComponentName(getPackageName(), NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
+                .setRequiredNetworkType(selectedNetworkOption) // network 상태 설정
+                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked()) // idle
+                .setRequiresCharging(mDeviceChargingSwitch.isChecked()); //charging
+
+        if (seekBarSet) {
+            builder.setOverrideDeadline(seekBarInteger * 1000);
+        }
+
+
+        boolean constraintSet = selectedNetworkOption !=
+                JobInfo.NETWORK_TYPE_NONE
+                || mDeviceChargingSwitch.isChecked()
+                || mDeviceIdleSwitch.isChecked()
+                || seekBarSet;
+
+
+        if(constraintSet) {
+            //Schedule the job and notify the user
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+            Toast.makeText(this, "Job Scheduled, job will run when " +
+                    "the constraints are met.", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Please set at least one constraint",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+
+    public void cancelJobs(View view) {
+        if(mScheduler != null){
+            mScheduler.cancelAll(); //mscheduler 가 null 이 아니면 다 취소 후 null 로 바꿈
+            mScheduler = null;
+            Toast.makeText(this,"Jobs cancelled", Toast.LENGTH_SHORT).show();
+        }
     }
 }
